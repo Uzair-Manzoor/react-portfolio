2d7036471a1b78316c522c012c8d316725458857
Uzair-Manzoor <uzair.kayani555@gmail.com>
2024-05-05 11:50:50 +0500

    add navbat utils component

---
+++ src/components/navBar/utils.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    label: 'HOME',
+    to: '/',
+  },
+  {
+    label: 'ABOUT',
+    to: '/about',
+  },
+  {
+    label: 'PORTFOLIO',
+    to: '/portfolio',
+  },
+  {
+    label: 'SKILLS',
+    to: '/skills',
+  },
+  {
+    label: 'CONTACT',
+    to: '/contact',
+  },
+];
+
+export default data;
