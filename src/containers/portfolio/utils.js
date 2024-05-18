be41255e003ff932b0faf9f66cda02cd8978787b
Uzair-Manzoor <uzair.kayani555@gmail.com>
2024-05-18 14:54:28 +0500

    update utils.js file to add spaceX project detail to the portfolio

--- src/containers/portfolio/utils.js
+++ src/containers/portfolio/utils.js
@@ -1,7 +1,15 @@
-// import Image from '../../images/image1.jpg';
+import ImageOne from '../../images/image1.webp';
 
 const portfolioData = [
-
+  {
+    id: 2,
+    name: 'Space Hub',
+    description: 'Space Hub is a cutting-edge web application integrated with real-time data from the SpaceX API, and empowers users to effortlessly book rockets and participate in curated space missions. Experience the future of space exploration with Space Hub.',
+    image: ImageOne,
+    link: 'https://space-travelers-8tvba23t2-uzair-manzoor.vercel.app/',
+    code: 'https://github.com/Uzair-Manzoor/SpaceTravelersHub.git',
+    tech: ['React', 'Redux', 'CSS5', 'API'],
+  },
 ];
 
 const filterData = [
