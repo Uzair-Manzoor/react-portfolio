136decbaa1b3061b4f65925def150dada87295fe
Uzair-Manzoor <uzair.kayani555@gmail.com>
2024-05-25 19:06:06 +0500

    removed index argument from map function

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -6,17 +6,27 @@ import './styles.scss';
 
 const SocialFooter = () => {
   const socialLinks = [
-    { name: 'GitHub', link: 'https://github.com/Uzair-Manzoor', icon: <FaGithub size={25} color="var(--theme-main-color)" /> },
-    { name: 'LinkedIn', link: 'https://www.linkedin.com/in/uzair-manzoor5', icon: <FaLinkedin size={25} color="var(--theme-main-color)" /> },
-    { name: 'Facebook', link: 'https://www.facebook.com/uzair.kiyani555/', icon: <FaFacebook size={25} color="var(--theme-main-color)" /> },
-    { name: 'E-mail', link: 'mailto:uzair.kayani555@gmail.com', icon: <FaEnvelope size={25} color="var(--theme-main-color)" /> },
-    { name: 'WhatsApp', link: 'https://wa.me/923450978605', icon: <FaWhatsapp size={25} color="var(--theme-main-color)" /> },
+    {
+      id: 'github', name: 'GitHub', link: 'https://github.com/Uzair-Manzoor', icon: <FaGithub size={25} color="var(--theme-main-color)" />,
+    },
+    {
+      id: 'linkedin', name: 'LinkedIn', link: 'https://www.linkedin.com/in/uzair-manzoor5', icon: <FaLinkedin size={25} color="var(--theme-main-color)" />,
+    },
+    {
+      id: 'facebook', name: 'Facebook', link: 'https://www.facebook.com/uzair.kiyani555/', icon: <FaFacebook size={25} color="var(--theme-main-color)" />,
+    },
+    {
+      id: 'email', name: 'E-mail', link: 'mailto:uzair.kayani555@gmail.com', icon: <FaEnvelope size={25} color="var(--theme-main-color)" />,
+    },
+    {
+      id: 'whatsapp', name: 'WhatsApp', link: 'https://wa.me/923450978605', icon: <FaWhatsapp size={25} color="var(--theme-main-color)" />,
+    },
   ];
 
   return (
     <ul className="social-footer">
-      {socialLinks.map((socialLink, index) => (
-        <li key={index}>
+      {socialLinks.map((socialLink) => (
+        <li key={socialLink.id}>
           <a className="link" href={socialLink.link} rel="noopener noreferrer" target="_blank">
             {socialLink.icon}
           </a>
